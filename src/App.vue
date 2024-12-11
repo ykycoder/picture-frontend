5a5189bd67991afbb51e629bc03a8a04a8f63390
ykycoder <649074096@qq.com>
2024-12-11 21:33:34 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <a-config-provider :locale="locale">
+      <BasicLayout />
+    </a-config-provider>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { healthUsingGet } from '@/api/mainController.ts'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
+
+let locale = zhCN;
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = "欢迎来到云图库~";
+</script>
+
+<style scoped>
+#homePage {
+
+}
+</style>
