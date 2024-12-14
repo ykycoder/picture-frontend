65b0fe18671acc53451b08a0f7d1ecb2b3e284ea
ykycoder <649074096@qq.com>
2024-12-14 17:18:41 +0800

    用户模块开发

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,22 @@
 
 <script setup lang="ts">
 import BasicLayout from '@/layouts/BasicLayout.vue'
-import { healthUsingGet } from '@/api/mainController.ts'
-import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
-import zhCN from 'ant-design-vue/es/locale/zh_CN';
-import dayjs from 'dayjs';
-import 'dayjs/locale/zh-cn';
-dayjs.locale('zh-cn');
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+// import { healthUsingGet } from '@/api/mainController.ts'
+// import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
 
-let locale = zhCN;
-const loginUserStore = useLoginUserStore()
-loginUserStore.fetchLoginUser()
+// 已经改为在权限校验文件中获取
+// const loginUserStore = useLoginUserStore()
+// loginUserStore.fetchLoginUser()
 
-healthUsingGet().then((res) => {
-  console.log(res)
-})
+// healthUsingGet().then((res) => {
+//   console.log(res)
+// })
+dayjs.locale('zh-cn')
+
+let locale = zhCN
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
